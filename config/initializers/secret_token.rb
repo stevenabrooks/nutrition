1547dc631a42145c725d9e98ef872aa40ba01c48
Steven Brooks <brooks.stevena@gmail.com>
2013-11-25 18:09:34 -0500

    started nutrition project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nutrition::Application.config.secret_token = 'c17a0a7cac894f6cb9f55e565167e4d0dfe14d97f742485c8262f22e0d2aed0b048afe6685bd0ccfedb951e7b87b54dd7e79c1d609705bdbbdf4d7e6936ef837'
