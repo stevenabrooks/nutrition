bbe14db152918f4405e3230eebb6223ab8efa215
Steven Brooks <brooks.stevena@gmail.com>
2013-11-26 11:56:11 -0500

    added testimonials to landing page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
   def index
+    @testimonials = Testimonial.order('created_at DESC').limit(2)
+    @t1 = @testimonials.first
+    @t2 = @testimonials.last
   end
 end
